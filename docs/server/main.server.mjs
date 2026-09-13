587eb1ea7ef1b8a4c0421d774b8b65e5c28e97f6
Mina Lotfy <minalotfysaad@gmail.com>
2026-09-13 17:42:11 +0300

    Configure GitHub Pages deployment via GitHub Actions and update docs build

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-MVTON4NF.mjs";import"./chunk-BUZZMFKE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/app/core/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  baseUrl: 'https://ecommerce.routemisr.com',
+  serverUrl: 'https://minalotfysaad.github.io/FreeCart-Ecommerce/',
+};
